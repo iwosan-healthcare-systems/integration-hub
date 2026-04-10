54072ccd8fb81af5e5f4eb7b0283a1d5b7a448b9
KhennyyB <khennyphresh@gmail.com>
2026-04-10 08:48:37 +0000

    Update hero banner asset

---
+++ src/layouts/HubLayout.tsx
@@ -0,0 +1,17 @@
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
+import { TopNavbar } from "@/components/TopNavbar";
+
+export function HubLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <div className="min-h-screen flex w-full">
+        <AppSidebar />
+        <div className="flex-1 flex flex-col min-w-0">
+          <TopNavbar />
+          <main className="flex-1 overflow-y-auto pb-8">{children}</main>
+        </div>
+      </div>
+    </SidebarProvider>
+  );
+}
