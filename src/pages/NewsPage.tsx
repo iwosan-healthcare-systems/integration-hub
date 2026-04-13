5fceebe65494b5d636ac59cd89a9761f95f519f3
KhennyyB <khennyphresh@gmail.com>
2026-04-13 12:41:17 +0100

    update news page

--- src/pages/NewsPage.tsx
+++ src/pages/NewsPage.tsx
@@ -9,7 +9,7 @@ import teamImg from "@/assets/team-photo.jpg";
 import innovationImg from "@/assets/innovation-bg.jpg";
 import iwosanAlaro from "@/assets/iwosan-alaro.jpg";
 
-const images = [hospitalImg, diagnosticsImg, teamImg, innovationImg, hospitalImg, diagnosticsImg, iwosanAlaro];
+const images = [iwosanAlaro, diagnosticsImg, teamImg, innovationImg, hospitalImg, diagnosticsImg];
 const categories = ["All", ...Array.from(new Set(newsItems.map((n) => n.category)))];
 
 const NewsPage = () => {
