3713072f63f2d30f1ee61b966ca4d6cf20a79082
khennyyb <khennyphresh@gmail.com>
2026-08-12 20:35:49 +0100

    fix: update SOP folder URL to correct SharePoint link

--- src/pages/ResourcesPage.tsx
+++ src/pages/ResourcesPage.tsx
@@ -3,7 +3,7 @@ import { subsidiaries } from "@/data/hub-data";
 import { ArrowRight, Building2, FileText, Shield, Users } from "lucide-react";
 import { Seo } from "@/components/Seo";
 
-const sopFolderUrl = "https://iwosan.sharepoint.com/:f:/s/IWOSAN-HEALTHCARE-SYSTEMS/IgCQRkxrc-ImRps1sQtEs4exAauTGCxHW-BFgF3cm_lsFgU";
+const sopFolderUrl = "https://iwosan.sharepoint.com/sites/IWOSAN-HEALTHCARE-SYSTEMS/SitePages/CollabHome.aspx";
 
 const sopFocusAreas = [
   {
